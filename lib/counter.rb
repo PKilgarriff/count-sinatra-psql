37f2840187d674527ee182481a1551e4c78679b7
Paul <pkilgarriff@gmail.com>
2022-03-14 21:11:01 +0000

    Make Counter#now a private method

--- lib/counter.rb
+++ lib/counter.rb
@@ -11,26 +11,28 @@ class Counter
     result[0]['time']
   end
 
-  def now(time = Time.now)
-    time
-  end
-
   def increment
     read_count = count
     result = DatabaseConnection.query("UPDATE counter SET count = '#{read_count + 1}', time = '#{now}' WHERE id=1;")
   end
-
+  
   def decrement
     read_count = count
     result = DatabaseConnection.query("UPDATE counter SET count = '#{read_count - 1}', time = '#{now}' WHERE id=1;")
   end
-
+  
   def reset
     read_count = count
     result = DatabaseConnection.query("UPDATE counter SET count = 0, time = '#{now}' WHERE id=1;")
   end
-
+  
   def self.instance
     @counter ||= Counter.new
+  end
+
+  private
+
+  def now(time = Time.now)
+    time
   end
 end
